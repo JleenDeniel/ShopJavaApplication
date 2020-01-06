51363221fd27ab6c1d828e3bd02668e80b98d687
JleenDeniel <79879013038@yandex.ru>
2020-01-07 00:45:12 +0300

    СТАБИЛЬНО работающий режим кассы и режим склада. Добавить еще режим работы с персоналом и хватит.

--- src/Controllers/StorageModeController.java
+++ src/Controllers/StorageModeController.java
@@ -1,61 +1,158 @@
 package Controllers;
 
+import java.net.URL;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ResourceBundle;
+
 import connectivity.ConnectionClass;
+import javafx.beans.property.SimpleStringProperty;
+import javafx.beans.value.ObservableValue;
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
 import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.Button;
+import javafx.scene.control.ComboBox;
+import javafx.scene.control.DatePicker;
+import javafx.scene.control.Label;
 import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableColumn.CellDataFeatures;
 import javafx.scene.control.TableView;
+import javafx.scene.control.TextField;
 import javafx.scene.control.cell.PropertyValueFactory;
+import javafx.scene.input.MouseEvent;
+import javafx.scene.paint.Color;
+import javafx.util.Callback;
 import shopLogic.Good;
 
-import java.sql.ResultSet;
-import java.sql.SQLException;
-
-public class StorageModeController {
-    public TableColumn<Good, String> goodNameCol;
-    public TableColumn<Good, String> goodPriceCol;
-    public TableColumn<Good, String> expireDateCol;
-    public TableColumn<Good, String> codeCol;
-    public TableColumn<Good, String> goodPlaceCol;
-    public TableColumn<Good, Integer> goodQuaintCol;
-    public TableColumn<Good, Integer> goodTypeCol;
+/**
+ * FXML Controller class
+ *
+ */
+public class StorageModeController implements Initializable {
+//todo https://devcolibri.com/javafx-2-%D0%BA%D0%B0%D0%BA-%D0%B2%D1%8B%D0%B2%D0%B5%D1%81%D1%82%D0%B8-%D0%B4%D0%B0%D0%BD%D0%BD%D1%8B%D0%B5-%D0%B2-tableview/
     @FXML
-    private TableView<Good> storageTable;
-
-    //ArrayList<Good> objectsFromDB = new ArrayList<>();
-    ObservableList<Good> goods = FXCollections.observableArrayList();
+    private TextField goodnameField;
+    @FXML
+    private TextField goodPriceField;
+    @FXML
+    private DatePicker goodExpireDatePicker;
+    @FXML
+    private TextField goodCode;
+    @FXML
+    private TextField goodPlaceField;
+    @FXML
+    private TextField goodQuantityField;
 
     @FXML
-    private void initialize() throws SQLException {
-        setStorage();
+    private TableColumn<Good, String> nameCol, priceCol, expireCol, codeCol, placeCol;
+    @FXML
+    private TableColumn<Good, Integer> quaintityCol, typeCol;
 
+    @FXML
+    private Button btnSave;
+    @FXML
+    private ComboBox<String> goodTypeBox;
+    @FXML
+    Label lblStatus;
 
-        goodNameCol.setCellFactory(new PropertyValueFactory("goodName"));
-        goodPriceCol.setCellFactory(new PropertyValueFactory("goodPrice"));
-        expireDateCol.setCellFactory(new PropertyValueFactory("goodExpireDate"));
-        codeCol.setCellFactory(new PropertyValueFactory("goodCode"));
-        goodPlaceCol.setCellFactory(new PropertyValueFactory("goodPlace"));
-        goodQuaintCol.setCellValueFactory(new PropertyValueFactory("goodQuantity"));
-        goodTypeCol.setCellValueFactory(new PropertyValueFactory("goodType"));
+    @FXML
+    TableView tblData;
 
-        storageTable.getColumns().setAll(goodNameCol, goodPriceCol, expireDateCol, codeCol, goodPlaceCol, goodQuaintCol, goodTypeCol);
-        storageTable = new TableView<>(goods);
+    private ObservableList<Good> storage = FXCollections.observableArrayList();
+
+    /**
+     * Initializes the controller class.
+     */
+    ConnectionClass connection = new ConnectionClass();
+
+    public StorageModeController() throws SQLException {
     }
 
+    @Override
+    public void initialize(URL url, ResourceBundle rb) {
+        goodTypeBox.getItems().addAll("1", "2", "3", "4", "5", "6", "7", "8");
+        initData();
+        tblData.setItems(storage);
 
-    public void setStorage() throws SQLException {
-        ConnectionClass connection = new ConnectionClass();
-        String sql = "Select * from goods;";
-        ResultSet res = connection.execQuery(sql);
-        // сделать большой запрос всей таблицы, идти пока res.next(), потом сохдать какую нибудь коллекцию и пушить
-        //в нее новые обьекты
-        while (res.next()){
-            goods.add(new Good(res.getString(2), res.getString(3), res.getString(4),
-                    res.getString(5), res.getString(6), res.getInt(7), res.getInt(8)));
+        nameCol.setCellValueFactory(new PropertyValueFactory<Good, String >("goodName"));
+        priceCol.setCellValueFactory(new PropertyValueFactory<Good, String >("goodPrice"));
+        expireCol.setCellValueFactory(new PropertyValueFactory<Good, String >("goodExpireDate"));
+        codeCol.setCellValueFactory(new PropertyValueFactory<Good, String >("goodCode"));
+        placeCol.setCellValueFactory(new PropertyValueFactory<Good, String >("goodPlace"));
+        quaintityCol.setCellValueFactory(new PropertyValueFactory<Good, Integer>("goodQuantity"));
+        typeCol.setCellValueFactory(new PropertyValueFactory<Good, Integer>("goodType"));
+
+    }
+
+    @FXML
+    private void HandleEvents(MouseEvent event) {
+        //check if not empty
+        if (goodCode.getText().isEmpty() || goodnameField.getText().isEmpty() || goodPriceField.getText().isEmpty() || goodExpireDatePicker.getValue().equals(null)) {
+            lblStatus.setTextFill(Color.TOMATO);
+            lblStatus.setText("Enter all details");
+        } else {
+            saveData();
+        }
+
+    }
+
+    private void clearFields() {
+        goodnameField.clear();
+        goodPriceField.clear();
+        goodCode.clear();
+        goodPlaceField.clear();
+        goodQuantityField.clear();
+    }
+
+    private String saveData() {
+
+        try {
+            connection.addToGoods(goodnameField.getText(), goodPriceField.getText(), setDateForConnector(goodExpireDatePicker.getValue().toString()), goodCode.getText(),
+                    goodPlaceField.getText(), Integer.parseInt(goodQuantityField.getText()), Integer.parseInt(goodTypeBox.getValue()));
+
+            lblStatus.setTextFill(Color.GREEN);
+            lblStatus.setText("Added Successfully");
+
+            clearFields();
+            return "Success";
+
+        } catch (SQLException ex) {
+            System.out.println(ex.getMessage());
+            lblStatus.setTextFill(Color.TOMATO);
+            lblStatus.setText(ex.getMessage());
+            return "Exception";
         }
     }
 
 
+    //only fetch columns
+    private void initData() {
+        String SQL = "SELECT * from goods";
+        try {
+            ResultSet res = connection.execQuery(SQL);
+            while(res.next()){
+                storage.add(new Good(res.getString(2), res.getString(3), res.getString(4),
+                        res.getString(5), res.getString(6), res.getInt(7), res.getInt(8)));
+            }
+
+        } catch (Exception e) {
+            System.out.println("Error " + e.getMessage());
+
+        }
+    }
+
+    public String setDateForConnector(String value){
+        String res = new String();
+        char[] year = new char[4];
+        char[] month = new char[2];
+        char[] day = new char[2];
+        value.getChars(6, 9, year, 0);
+        value.getChars(3, 4, month, 0);
+        value.getChars(0, 1, day, 0);
+        res.join( year.toString(),"-", month.toString(),"-", day.toString());
+        return res;
+    }
 
 }
--- src/shopLogic/Good.java
+++ src/shopLogic/Good.java
@@ -6,69 +6,48 @@ import javafx.beans.property.SimpleStringProperty;
 import javafx.beans.property.StringProperty;
 
 public class Good {
-    private StringProperty goodName;
-    private StringProperty goodPrice;
-    private StringProperty goodExpireDate;
-    private StringProperty goodCode;
-    private StringProperty goodPlace;
+    private String goodName;
+    private String goodPrice;
+    private String goodExpireDate;
+    private String goodCode;
+    private String goodPlace;
 
-    private IntegerProperty goodQuantity;
-    private IntegerProperty goodType;
+    private Integer goodQuantity;
+    private Integer goodType;
 
 
     public Good(String goodName, String goodPrice, String goodExpireDate, String goodCode, String goodPlace, Integer goodQuantity, Integer goodType) {
-        this.goodName = new SimpleStringProperty(goodName);
-        this.goodPrice = new SimpleStringProperty(goodPrice);
-        this.goodExpireDate = new SimpleStringProperty(goodExpireDate);
-        this.goodCode = new SimpleStringProperty(goodCode);
-        this.goodPlace = new SimpleStringProperty(goodPlace);
-        this.goodQuantity = new SimpleIntegerProperty(goodQuantity);
-        this.goodType = new SimpleIntegerProperty(goodType);
+        this.goodName = goodName;
+        this.goodPrice = goodPrice;
+        this.goodExpireDate = goodExpireDate;
+        this.goodCode = goodCode;
+        this.goodPlace = goodPlace;
+        this.goodQuantity = goodQuantity;
+        this.goodType = goodType;
     }
 
-    public void setGoodName(String value){goodNameProperty().set(value); }
-    public String getGoodName(){return goodNameProperty().get(); }
-    public StringProperty goodNameProperty(){
-        if(goodName == null) goodName = new SimpleStringProperty(this, "goodName");
-        return goodName;
-    }
+    public void setGoodName(String value){goodName = value; }
+    public String getGoodName(){return goodName; }
 
-    public void setGoodPrice(String value){goodPriceProperty().set(value); }
-    public String getGoodPrice(){return goodPriceProperty().get(); }
-    public StringProperty goodPriceProperty(){
-        if(goodPrice == null) goodPrice = new SimpleStringProperty(this, "goodPrice");
-        return goodPrice;
-    }
 
-    public void setGoodExpireDate(String value){goodExpireDateProperty().set(value); }
-    public String getGoodExpireDate(){return goodExpireDateProperty().get(); }
-    public StringProperty goodExpireDateProperty(){
-        if(goodExpireDate == null) goodExpireDate = new SimpleStringProperty(this, "goodExpireDate");
-        return goodExpireDate;
-    }
-    public void setGoodCode(String value){goodCodeProperty().set(value); }
-    public String getGoodCode(){return goodCodeProperty().get(); }
-    public StringProperty goodCodeProperty(){
-        if(goodCode == null) goodCode = new SimpleStringProperty(this, "goodCode");
-        return goodCode;
-    }
-    public void setGoodPlace(String value){goodPlaceProperty().set(value); }
-    public String getGoodPlace(){return goodPlaceProperty().get(); }
-    public StringProperty goodPlaceProperty(){
-        if(goodPlace == null) goodPlace = new SimpleStringProperty(this, "goodPlace");
-        return goodPlace;
-    }
-    public void setGoodQuantity(int value){goodQuantityProperty().set(value); }
-    public int getGoodQuantity(){return goodQuantityProperty().get(); }
-    public IntegerProperty goodQuantityProperty(){
-        if(goodQuantity == null) goodQuantity = new SimpleIntegerProperty(this, "goodQuantity");
-        return goodQuantity;
-    }
-    public void setGoodType(int value){goodTypeProperty().set(value); }
-    public int getGoodType(){return goodTypeProperty().get(); }
-    public IntegerProperty goodTypeProperty(){
-        if(goodType == null) goodType = new SimpleIntegerProperty(this, "goodType");
-        return goodType;
-    }
+    public void setGoodPrice(String value){goodPrice = value; }
+    public String getGoodPrice(){return goodPrice; }
+
+
+    public void setGoodExpireDate(String value){goodExpireDate = value; }
+    public String getGoodExpireDate(){return goodExpireDate; }
+
+    public void setGoodCode(String value){goodCode = value; }
+    public String getGoodCode(){return goodCode; }
+
+    public void setGoodPlace(String value){goodPlace = value; }
+    public String getGoodPlace(){return goodPlace; }
+
+    public void setGoodQuantity(int value){goodQuantity = value; }
+    public int getGoodQuantity(){return goodQuantity; }
+
+    public void setGoodType(int value){goodType = value; }
+    public int getGoodType(){return goodType;}
+
 
 }
